b43936e95c817ecdbfb17cc65f8075f22a49cf43
George Ramirez <georgeramirez@georges-mbp.gateway.pace.com>
2016-10-16 17:47:23 -0500

    step one set up

--- main.js
+++ main.js
@@ -5,3 +5,28 @@
 // Running it will start the game
 // App should END when you guess the word correct
 // ----------------------------------------------------
+
+var inquirer = require('inquirer');
+var game = require('./game.js');
+var letter = require('./letter.js');
+var word = require('./word.js');
+var time = new Date().toDateString();
+
+function startGame (){
+
+    // Print instructions on screen
+    console.log('\n***** Hello and welcome to HANGMAN 10.16.GR, today is '+time+' *****\n\nThe words you will be trying to guess in this game are under the topic:\n\n"NAMES AND NICKNAMES FOR A RUSTIC OR COUNTRIFIED PERSON"\n(from the Dictionary of American Regional English)\n\n');
+    // Print blanks on screen
+    game.
+    // Print number of guesses left
+    // Print wrong guesses
+
+    // replace blank with correct guess
+    // Message you loose if your count gets to 10
+    // Message you win if all your guesses match the word selected 
+
+
+
+
+}; /* END of startGame function */
+startGame();
